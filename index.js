ccf5e941a8601c1a73706be080a4cb588fe3f76b
Meggyis <katalinsimon1413@gmail.com>
2025-08-11 16:13:22 +0200

    Added basic form handling and typewriter

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("Hello World")
+function generatePoem(event){
+event.preventDefault();
+new Typewriter('#poem', {
+  strings: ['Harminckét éves lettem én-'],
+  autoStart: true,
+  delay:1,
+  cursor:"",
+});
+}
+let poemFormElement=document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
